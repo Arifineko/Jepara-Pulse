de00dd1283ce819d549a83e8907beed8e9d894f7
ArifinEko <aeko86869@gmail.com>
2024-04-02 16:38:16 +0700

    update (image)
    image-destinations

--- data/cards.js
+++ data/cards.js
@@ -2,7 +2,7 @@ export const cards = [
     {
         id: "id1",
         name: 'Beach',
-        image: "assets/destination-1.png",
+        image: "assets/image/destination-1.png",
         catagory: "Travel backpack",
         information: "Jepara Regency is renowned for its exquisite wooden carvings and offers a range of attractions, including the stunning Karimunjawa Archipelago and the picture...",
         imageUploader: "assets/Avatar.png",
@@ -12,7 +12,7 @@ export const cards = [
     {
         id: "id1",
         name: 'Beach',
-        image: "assets/destination-1.png",
+        image: "assets/image/destination-2.png",
         catagory: "Travel backpack",
         information: "Jepara Regency is renowned for its exquisite wooden carvings and offers a range of attractions, including the stunning Karimunjawa Archipelago and the picture...",
         imageUploader: "assets/Avatar.png",
@@ -22,11 +22,72 @@ export const cards = [
     {
         id: "id1",
         name: 'Beach',
-        image: "assets/destination-1.png",
+        image: "assets/image/destination-3.png",
         catagory: "Travel backpack",
         information: "Jepara Regency is renowned for its exquisite wooden carvings and offers a range of attractions, including the stunning Karimunjawa Archipelago and the picture...",
         imageUploader: "assets/Avatar.png",
         nameUploader: "Dinas Pariwisata Jepara",
         date: "22 Jan 2022"
-    }
+    },
+    {
+        id: "id1",
+        name: 'Beach',
+        image: "assets/image/destination-4.png",
+        catagory: "Travel backpack",
+        information: "Jepara Regency is renowned for its exquisite wooden carvings and offers a range of attractions, including the stunning Karimunjawa Archipelago and the picture...",
+        imageUploader: "assets/Avatar.png",
+        nameUploader: "Dinas Pariwisata Jepara",
+        date: "22 Jan 2022"
+    },
+    {
+        id: "id1",
+        name: 'Beach',
+        image: "assets/image/destination-5.png",
+        catagory: "Travel backpack",
+        information: "Jepara Regency is renowned for its exquisite wooden carvings and offers a range of attractions, including the stunning Karimunjawa Archipelago and the picture...",
+        imageUploader: "assets/Avatar.png",
+        nameUploader: "Dinas Pariwisata Jepara",
+        date: "22 Jan 2022"
+    },
+    {
+        id: "id1",
+        name: 'Beach',
+        image: "assets/image/destination-6.png",
+        catagory: "Travel backpack",
+        information: "Jepara Regency is renowned for its exquisite wooden carvings and offers a range of attractions, including the stunning Karimunjawa Archipelago and the picture...",
+        imageUploader: "assets/Avatar.png",
+        nameUploader: "Dinas Pariwisata Jepara",
+        date: "22 Jan 2022"
+    },
+    {
+        id: "id1",
+        name: 'Beach',
+        image: "assets/image/destination-7.png",
+        catagory: "Travel backpack",
+        information: "Jepara Regency is renowned for its exquisite wooden carvings and offers a range of attractions, including the stunning Karimunjawa Archipelago and the picture...",
+        imageUploader: "assets/Avatar.png",
+        nameUploader: "Dinas Pariwisata Jepara",
+        date: "22 Jan 2022"
+    },
+    {
+        id: "id1",
+        name: 'Beach',
+        image: "assets/image/destination-8.png",
+        catagory: "Travel backpack",
+        information: "Jepara Regency is renowned for its exquisite wooden carvings and offers a range of attractions, including the stunning Karimunjawa Archipelago and the picture...",
+        imageUploader: "assets/Avatar.png",
+        nameUploader: "Dinas Pariwisata Jepara",
+        date: "22 Jan 2022"
+    },
+    {
+        id: "id1",
+        name: 'Beach',
+        image: "assets/image/destination-9.png",
+        catagory: "Travel backpack",
+        information: "Jepara Regency is renowned for its exquisite wooden carvings and offers a range of attractions, including the stunning Karimunjawa Archipelago and the picture...",
+        imageUploader: "assets/Avatar.png",
+        nameUploader: "Dinas Pariwisata Jepara",
+        date: "22 Jan 2022"
+    },
+
 ]
